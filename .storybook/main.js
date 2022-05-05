1980121384ea88f05d585b9bbd6c43b3dc22367d
rayshen <rayshen@qnap.com>
2022-05-05 09:56:03 +0800

    add @svgr/webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,30 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react"
-}
+  framework: "@storybook/react",
+  webpackFinal: async (config) => {
+    config.module.rules.unshift({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: {
+                removeDimensions: true,
+              },
+            },
+          },
+        },
+      ],
+    });
+    return config;
+  },
+};
